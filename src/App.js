5835631ca6898617830e59aec179604cff64d83a
Huang-Ming Chang <huang.ming.chang@exact.com>
2018-02-12 17:52:49 +0100

    Minor updates; test commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+    import './App.css';
 
-    const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+const App = () => (
+    <div className='App'>
+        <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-titl'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
             To get started, edit <code>src/App.js</code> and save to reload.
         </p>
     </div>
