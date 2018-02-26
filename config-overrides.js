c2208d4f8e7bc5f2b366a153754bddf790acf4e4
SimpleFrontend <huang-mingchang@LT-17-055.local>
2018-02-27 00:18:44 +0100

    install antd and inject less with react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const {injectBabelPlugin} = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(_config, env) {
+    let config = _config;
+    config = injectBabelPlugin(
+        ['import', {libraryName: 'antd', libraryDirectory: 'es', style: true}],
+        config
+    );
+    config = rewireLess.withLoaderOptions({
+        modifyVars: {
+            '@primary-color': '#ce6872'
+        }
+    })(config, env);
+    return config;
+};
