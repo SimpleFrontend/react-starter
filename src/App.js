26904ac7aed0e2c037391acd5b242c6ced3256fd
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-05-07 13:02:40 +0200

    Applied emotion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { DatePicker } from 'antd';
+
+import Container from './Container';
 import logo from './logo.svg';
-import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-titl">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    <div>
+  <Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-titl">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
       <DatePicker />
     </div>
-  </div>
+  </Container>
 );
 
 export default App;
